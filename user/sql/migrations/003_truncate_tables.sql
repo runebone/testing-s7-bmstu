e21c3467f0974a2ec839cf6483ada2e12a8a1c97
Konstantin Runov <runebone1@gmail.com>
2024-09-15 16:54:57 +0300

    lab3: update migrations

--- user/sql/migrations/003_truncate_tables.sql
+++
@@ -1,6 +0,0 @@
--- Disable foreign keys temporarily to avoid conflicts
-SET session_replication_role = replica;
-
-TRUNCATE TABLE users RESTART IDENTITY CASCADE;
-
-SET session_replication_role = DEFAULT;

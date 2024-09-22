df27ba84c8eb0005e0847e302ea67689eb7f5036
Konstantin Runov <runebone1@gmail.com>
2024-09-22 19:48:08 +0300

    lab3: todo: add basic migrations

---
+++ todo/migrations/sql/001_create_tables.up.sql
@@ -0,0 +1,31 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE boards (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID NOT NULL, -- From user service
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE columns (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    board_id UUID REFERENCES boards(id) ON DELETE CASCADE,
+    user_id UUID NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    position REAL NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE cards (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    column_id UUID REFERENCES columns(id) ON DELETE CASCADE,
+    user_id UUID NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    position REAL NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);

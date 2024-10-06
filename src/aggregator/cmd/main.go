3a374257904648d2adf2389fb73d27850cc67b7d
Konstantin Runov <runebone1@gmail.com>
2024-10-06 21:49:50 +0300

    agg: add config and dockerfiles

--- src/aggregator/cmd/main.go
+++ src/aggregator/cmd/main.go
@@ -1,1 +1,66 @@
 package main
+
+import (
+	"aggregator/internal/adapter/logger"
+	"aggregator/internal/middleware"
+	"fmt"
+
+	httpAuth "aggregator/internal/adapter/service/auth/http"
+	httpTodo "aggregator/internal/adapter/service/todo/http"
+	httpUser "aggregator/internal/adapter/service/user/http"
+	api "aggregator/internal/api/v1"
+	"aggregator/internal/config"
+	h "aggregator/internal/handler/v1"
+	"aggregator/internal/service/auth"
+	"aggregator/internal/service/todo"
+	"aggregator/internal/service/user"
+	v1 "aggregator/internal/usecase/v1"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	config, err := config.LoadConfig("config.toml")
+	if err != nil {
+		log.Println("Error reading config (config.toml)")
+	}
+
+	var userSvc user.UserService
+	{
+		baseURL := fmt.Sprintf("http://%s:%d/%s", config.User.ContainerName, config.User.LocalPort, config.User.BaseURL)
+		logger := logger.NewZapLogger(config.User.Log)
+		userSvc = httpUser.NewUserService(baseURL, 2, logger)
+	}
+
+	var authSvc auth.AuthService
+	{
+		baseURL := fmt.Sprintf("http://%s:%d/%s", config.Auth.ContainerName, config.Auth.LocalPort, config.Auth.BaseURL)
+		logger := logger.NewZapLogger(config.Auth.Log)
+		authSvc = httpAuth.NewAuthService(baseURL, 2, logger)
+	}
+
+	var todoSvc todo.TodoService
+	{
+		baseURL := fmt.Sprintf("http://%s:%d/%s", config.Todo.ContainerName, config.Todo.LocalPort, config.Todo.BaseURL)
+		logger := logger.NewZapLogger(config.Todo.Log)
+		todoSvc = httpTodo.NewTodoService(baseURL, 2, logger)
+	}
+
+	logger := logger.NewZapLogger(config.Aggregator.Log)
+	uc := v1.NewAggregatorUseCase(userSvc, authSvc, todoSvc)
+	handler := h.NewAggregatorHandler(uc)
+
+	router := mux.NewRouter()
+	loggingMiddleware := middleware.NewLoggingMiddleware(logger)
+	// authMiddleware := middleware.NewAuthMiddleware(authSvc)
+	router.Use(loggingMiddleware.Middleware)
+	// router.Use(authMiddleware.Middleware)
+	api.InitializeV1Routes(router, handler)
+
+	port := fmt.Sprintf("%d", config.Aggregator.LocalPort)
+
+	log.Printf("Starting server on :%s\n", port)
+	http.ListenAndServe(":"+port, router)
+}

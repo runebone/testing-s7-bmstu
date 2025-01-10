df1530d98f3b5a519e4be4383fefe4996b754c85
Konstantin Runov <runebone1@gmail.com>
2025-01-10 14:46:51 +0300

    todo: test UpdateBoard

--- src/todo/internal/usecase/v1/todo_test.go
+++ src/todo/internal/usecase/v1/todo_test.go
@@ -768,3 +768,71 @@ func TestGetNewCards(t *testing.T) {
 		}
 	})
 }
+
+func TestUpdateBoard(t *testing.T) {
+	runner.Run(t, "TestUpdateBoard", func(pt provider.T) {
+		tests := []struct {
+			name      string
+			board     entity.Board
+			mockSetup func(mockBoardRepo *mocks.BoardRepository, board *entity.Board)
+			wantErr   bool
+			err       error
+		}{
+			{
+				name: "positive",
+				board: entity.Board{
+					ID:     uuid.New(),
+					UserID: uuid.New(),
+					Title:  "PositiveBoard",
+				},
+				mockSetup: func(mockBoardRepo *mocks.BoardRepository, board *entity.Board) {
+					mockBoardRepo.On("UpdateBoard", context.Background(), board).Return(nil)
+				},
+				wantErr: false,
+			},
+			{
+				name: "negative",
+				board: entity.Board{
+					ID:     uuid.New(),
+					UserID: uuid.New(),
+					Title:  "NegativeBoard",
+				},
+				mockSetup: func(mockBoardRepo *mocks.BoardRepository, board *entity.Board) {
+					mockBoardRepo.On("UpdateBoard", context.Background(), board).Return(errors.New(""))
+				},
+				wantErr: true,
+				err:     v1.ErrUpdateBoard,
+			},
+		}
+
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				t.Parallel()
+
+				runner.Run(t, tt.name, func(pt provider.T) {
+					mockBoardRepo := new(mocks.BoardRepository)
+					mockColumnRepo := new(mocks.ColumnRepository)
+					mockCardRepo := new(mocks.CardRepository)
+					logger := log.NewEmptyLogger()
+
+					uc := v1.NewTodoUseCase(mockBoardRepo, mockColumnRepo, mockCardRepo, logger)
+
+					tt.mockSetup(mockBoardRepo, &tt.board)
+
+					pt.WithNewStep("Call UpdateBoard", func(sCtx provider.StepCtx) {
+						err := uc.UpdateBoard(context.Background(), &tt.board)
+
+						if tt.wantErr {
+							sCtx.Assert().Error(err, "Expected error")
+							sCtx.Assert().ErrorIs(err, tt.err)
+						} else {
+							sCtx.Assert().NoError(err, "Expected no error")
+						}
+
+						mockCardRepo.AssertExpectations(t)
+					})
+				})
+			})
+		}
+	})
+}

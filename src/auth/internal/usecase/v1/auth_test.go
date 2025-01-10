9e88009d1135638af32ad52d7dcb811c7fa2d17c
Konstantin Runov <runebone1@gmail.com>
2025-01-10 16:14:56 +0300

    auth: test Login

--- src/auth/internal/usecase/v1/auth_test.go
+++ src/auth/internal/usecase/v1/auth_test.go
@@ -105,6 +105,88 @@ func TestRegister(t *testing.T) {
 	})
 }
 
+func TestLogin(t *testing.T) {
+	runner.Run(t, "TestLogin", func(pt provider.T) {
+		tests := []struct {
+			name         string
+			email        string
+			password     string
+			accessToken  string
+			refreshToken string
+			mockSetup    func(mockTokenRepo *mocks.TokenRepository, mockUserSvc *mocks.UserService, mockTokenSvc *mocks.TokenService, email, password, accessToken, refreshToken string)
+			wantErr      bool
+			err          error
+		}{
+			{
+				name:         "positive",
+				email:        "positive@email.com",
+				password:     "P0s1t1v3P@ssw0rD",
+				accessToken:  "PositiveAccessToken",
+				refreshToken: "PositiveRefreshToken",
+				mockSetup: func(mockTokenRepo *mocks.TokenRepository, mockUserSvc *mocks.UserService, mockTokenSvc *mocks.TokenService, email, password, accessToken, refreshToken string) {
+					userID := uuid.New()
+					role := "user"
+					pwdHash, _ := hashPassword(password)
+					userDTO := &dto.User{
+						ID:           userID,
+						Email:        email,
+						Role:         role,
+						PasswordHash: pwdHash,
+					}
+
+					mockUserSvc.On("GetUserByEmail", context.Background(), email).Return(userDTO, nil)
+					mockTokenSvc.On("GenerateAccessToken", context.Background(), userID.String(), role).Return(accessToken, nil)
+					mockTokenSvc.On("GenerateRefreshToken", context.Background(), userID.String(), role).Return(refreshToken, nil)
+					mockTokenRepo.On("Save", context.Background(), mock.Anything).Return(nil)
+				},
+				wantErr: false,
+			},
+			{
+				name:         "negative",
+				email:        "negative@email.com",
+				password:     "N3g@t1v3P@ssw0rD",
+				accessToken:  "NegativeAccessToken",
+				refreshToken: "NegativeRefreshToken",
+				mockSetup: func(mockTokenRepo *mocks.TokenRepository, mockUserSvc *mocks.UserService, mockTokenSvc *mocks.TokenService, email, password, accessToken, refreshToken string) {
+					mockUserSvc.On("GetUserByEmail", context.Background(), email).Return(nil, errors.New(""))
+				},
+				wantErr: true,
+				err:     v1.ErrGetUserByEmail,
+			},
+		}
+
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				t.Parallel()
+
+				runner.Run(t, tt.name, func(pt provider.T) {
+					mockTokenRepo := new(mocks.TokenRepository)
+					mockUserSvc := new(mocks.UserService)
+					mockTokenSvc := new(mocks.TokenService)
+					logger := log.NewEmptyLogger()
+
+					uc := v1.NewAuthUseCase(mockTokenRepo, mockUserSvc, mockTokenSvc, logger)
+
+					tt.mockSetup(mockTokenRepo, mockUserSvc, mockTokenSvc, tt.email, tt.password, tt.accessToken, tt.refreshToken)
+
+					pt.WithNewStep("Call Login", func(sCtx provider.StepCtx) {
+						_, err := uc.Login(context.Background(), tt.email, tt.password)
+
+						if tt.wantErr {
+							sCtx.Assert().Error(err, "Expected error")
+							sCtx.Assert().ErrorIs(err, tt.err)
+						} else {
+							sCtx.Assert().NoError(err, "Expected no error")
+						}
+
+						mockUserSvc.AssertExpectations(t)
+					})
+				})
+			})
+		}
+	})
+}
+
 func hashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(bytes), err

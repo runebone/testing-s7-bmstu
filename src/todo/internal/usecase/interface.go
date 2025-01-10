1265115315408b9f0918b9dd97864e5701d09e64
Konstantin Runov <runebone1@gmail.com>
2025-01-10 13:54:39 +0300

    todo: sort out interface by CRUD

--- src/todo/internal/usecase/interface.go
+++ src/todo/internal/usecase/interface.go
@@ -10,21 +10,22 @@ import (
 
 type TodoUseCase interface {
 	CreateBoard(ctx context.Context, board *entity.Board) error
+	CreateColumn(ctx context.Context, column *entity.Column) error
+	CreateCard(ctx context.Context, card *entity.Card) error
+
 	GetBoardByID(ctx context.Context, id uuid.UUID) (*entity.Board, error)
 	GetBoardsByUser(ctx context.Context, userID uuid.UUID, limit, offset int) ([]entity.Board, error)
-	UpdateBoard(ctx context.Context, board *entity.Board) error
-	DeleteBoard(ctx context.Context, id uuid.UUID) error
-
-	CreateColumn(ctx context.Context, column *entity.Column) error
 	GetColumnByID(ctx context.Context, id uuid.UUID) (*entity.Column, error)
 	GetColumnsByBoard(ctx context.Context, boardID uuid.UUID, limit, offset int) ([]entity.Column, error)
-	UpdateColumn(ctx context.Context, column *entity.Column) error
-	DeleteColumn(ctx context.Context, id uuid.UUID) error
-
-	CreateCard(ctx context.Context, card *entity.Card) error
 	GetCardByID(ctx context.Context, id uuid.UUID) (*entity.Card, error)
 	GetCardsByColumn(ctx context.Context, columnID uuid.UUID, limit, offset int) ([]entity.Card, error)
 	GetNewCards(ctx context.Context, from, to time.Time) ([]entity.Card, error)
+
+	UpdateBoard(ctx context.Context, board *entity.Board) error
+	UpdateColumn(ctx context.Context, column *entity.Column) error
 	UpdateCard(ctx context.Context, card *entity.Card) error
+
+	DeleteBoard(ctx context.Context, id uuid.UUID) error
+	DeleteColumn(ctx context.Context, id uuid.UUID) error
 	DeleteCard(ctx context.Context, id uuid.UUID) error
 }
